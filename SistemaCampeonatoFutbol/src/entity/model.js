6ab642d1f5b14935f157ac04a8457f211265755b
GabrielCarriel1 <ccarrielm@unemi.edu.ec>
2023-08-07 13:02:37 -0500

    xd

--- SistemaCampeonatoFutbol/src/entity/model.js
+++ SistemaCampeonatoFutbol/src/entity/model.js
@@ -3,3 +3,35 @@ class campeonato {
     this.nombre = nombre;
   }
 }
+class EquipoFutbol {
+  constructor(
+    sede,
+    nombre,
+    fechaCreacion,
+    categoría,
+    escudo,
+    coloresedelEquipo,
+    alineacion,
+    estretegia
+  ) {
+    this.sede = sede;
+    this.nombre = nombre;
+    this.fechaCreacion = new Date(fechaCreacion);
+    this.categoría = categoría;
+    this.escudo = escudo;
+    this.coloresedelEquipo = coloresedelEquipo;
+    this.alineacion = alineacion;
+    this.estretegia = estretegia;
+  }
+}
+
+class Jugador {
+  constructor(nombres, apellidos, fechaNacimiento, posicion, numeroCamiseta) {
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+    this.fechaNacimiento = fechaNacimiento;
+    this.posicion = posicion;
+    this.numeroCamiseta = numeroCamiseta;
+  }
+}
+class Arbitro {}
